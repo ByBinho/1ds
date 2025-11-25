85bfd50a4599ce03e3d1943c4e58def0911843c2
ByBinho <fabioljfilho@gmail.com>
2025-11-24 22:56:17 -0300

    Update Tabuada.java

--- tpa/Tabuada.java
+++ tpa/Tabuada.java
@@ -1,27 +1,24 @@
 package lista;
+
 import java.util.Scanner;
 
 public class Tabuada {
     public static void main(String[] args) {
-        
-        Scanner in = new Scanner(System.in);
 
+        Scanner in = new Scanner(System.in);
         final int TAM = 5;
-        int A[];
-        int i, j;
+
+        int A[], i, j;
 
         A = new int[TAM];
 
-        
         for(i = 0; i < TAM; i++) {
-            System.out.println("Digite o " + (i+1) + "º valor do vetor A:");
+            System.out.println("Digite o " + (i+1) + " valor do vetor A:");
             A[i] = in.nextInt();
         }
 
-        
         for(i = 0; i < TAM; i++) {
             System.out.println("\nTabuada do " + A[i] + ":");
-
             for(j = 1; j <= 10; j++) {
                 System.out.println(A[i] + " x " + j + " = " + (A[i] * j));
             }
